5562251bb0c4faddd2d0b559681f16e43034852d
YorkeF <113220697+YorkeF@users.noreply.github.com>
2022-09-21 22:37:26 -0500

    Project Euler for CS 2133

---
+++ src/Problem48.java
@@ -0,0 +1,22 @@
+/*
+
+
+The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+
+Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
+
+ */
+
+import java.math.BigInteger;
+public class Problem48 {
+    public static void main(String[] args) {
+        BigInteger total = BigInteger.valueOf(0);
+        BigInteger current;
+        for (int i = 1; i < 1001; i++) {
+            current = BigInteger.valueOf(i).pow(i);
+            total = total.add(current);
+        }
+        total = total.mod(BigInteger.valueOf(1000000000).multiply(BigInteger.valueOf(10)));
+        System.out.println(total);
+    }
+}
